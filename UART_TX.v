565458bbd4f90fd49ba13c72fd6eb17670e5ef51
unknown <marijan.simundic-bendic.02@fesb.hr>
2022-05-22 22:46:06 +0200

    Initial commit

---
+++ UART_TX.v
@@ -0,0 +1,110 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// 
+//
+// 8-bit UART transmiter, no parity bit
+//
+//
+//////////////////////////////////////////////////////////////////////////////////
+module UART_TX(
+	input clk, enable,
+	input [7:0] data_in,
+	output reg tx, done, test,
+	output reg [1:0] test_state
+	);
+
+	parameter state_idle = 2'b00;
+	parameter state_start = 2'b01;
+	parameter state_data = 2'b10;
+	parameter state_stop = 2'b11;
+	parameter clk_per_bit = 32; //Must be set correctly: 
+										 //clk_per_bit = clk_frequency / baud_rate
+										 //max is 255
+
+
+	reg [7:0] clk_count;
+	reg [2:0] index;
+	reg [7:0] data_temp;
+	reg [1:0] state; 
+	
+	
+	initial begin
+		done <= 0;
+		tx <= 1;
+		test <= 0;
+		state <= state_idle;
+	end 
+	  
+	always@(posedge clk) begin 
+		case(state)
+			state_idle: begin
+				tx <= 1;
+				done <= 0;
+				clk_count <= 0;
+				index <= 0;
+				if(enable) begin
+					data_temp <= data_in;
+					state <= state_start; 
+				end
+				else
+					state <= state_idle;
+			end
+			
+			state_start: begin
+				tx <= 0;
+				if(clk_count <= clk_per_bit - 1) begin
+					clk_count <= clk_count + 1;
+				end
+				else begin
+					clk_count <= 0;
+					state <= state_data;
+				end
+			end
+			
+			state_data: begin
+				tx <= data_temp[index];
+				if(clk_count < clk_per_bit - 1) begin
+					clk_count <= clk_count + 1;
+					state <= state_data;
+				end
+				else begin
+					clk_count <= 0;
+					if(index < 7) begin
+						index <= index + 1;
+						state <= state_data;
+					end
+					else begin
+						index <= 0;
+						state <= state_stop;
+					end
+				end
+			end
+			
+			state_stop: begin
+				tx <= 1;
+				if (clk_count < clk_per_bit - 1) begin
+					clk_count <= clk_count + 1;
+					state <= state_stop;
+				end
+				else begin
+					done <= 1;
+					clk_count <= 0;
+					state <= state_idle;
+				end
+			end
+			
+			default: begin
+				state <= state_idle;
+				index <= 0;
+				clk_count <= 0;
+				tx <= 1;
+			end
+		endcase
+	end
+	
+	always@(posedge clk) begin
+		test_state <= state;
+	end
+	
+	
+endmodule
---
+++ UART_TX_test.v
@@ -0,0 +1,67 @@
+`timescale 1ns / 1ps
+
+////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer:
+//
+// Create Date:   16:19:35 05/22/2022
+// Design Name:   UART_TX
+// Module Name:   C:/Xilinx/UART/UART_TX_test.v
+// Project Name:  UART
+// Target Device:  
+// Tool versions:  
+// Description: 
+//
+// Verilog Test Fixture created by ISE for module: UART_TX
+//
+// Dependencies:
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+////////////////////////////////////////////////////////////////////////////////
+
+module UART_TX_test;
+
+	// Inputs
+	reg clk;
+	reg enable;
+	reg [7:0] data_in;
+
+	wire tx;
+	wire [1:0] test_state;
+	wire done; 
+	wire test;
+
+	// Instantiate the Unit Under Test (UUT)
+	UART_TX uut (
+		.clk(clk), 
+		.enable(enable),
+		.data_in(data_in),
+		.tx(tx),
+		.test_state(test_state),
+		.done(done),
+		.test(test)
+	); 
+
+	initial begin
+		// Initialize Inputs
+		clk = 0; 
+		enable = 0;
+		data_in <= 8'b10101010;
+		#10 enable = 1;
+		#10 enable = 0;
+		// Wait 100 ns for global reset to finish
+		#100; 
+        
+		// Add stimulus here
+		#1000 $stop;
+	end
+   
+	always begin
+		#1 clk <= ~clk;
+	end
+	
+endmodule
+
